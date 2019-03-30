a328c66335bcf3c75510e1005ef271274b5b7387
Sachin Mahajan <sachinm78@gmail.com>
2019-03-30 00:13:43 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -41,81 +41,39 @@ class MusicLibraryController
   end
 
   def list_songs
-    Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
-        puts "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
-      end
-    songs_sorted_by_name = Song.all.sort_by do |song|
-      song.name
-    end
-    songs_sorted_by_name.each.with_index(1) do |song,index|
-      puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+    Song.all.each_with_index {|song,index|puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
+  end
+
+   def list_artists
+    Artist.all.each {|artist| puts artist.name}
+  end
+
+   def list_genres
+    Genre.all.each {|genre| puts genre.name}
+  end
+
+   def play_song
+    song = Song.all[gets.to_i - 1]
+    puts "Playing #{song.artist.name} - #{song.name} - #{song.genre.name}"
+  end
+
+   def list_artist
+    puts "Enter artist"
+    specific_artist = gets.chomp
+    if Artist.find_by_name(specific_artist) != nil
+      Artist.find_by_name(specific_artist).songs.each {|song| puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"}
+    else
+      puts "Artist does not exist"
     end
   end
 
-  def list_artists
-    #prints all artists in the music library in a numbered list (alphabetized by artist name
-    songs_sorted_by_artist = Artist.all.sort_by do |artist| #returns an array of artists sorted by artist name
-      artist.name
-    end
-    songs_sorted_by_artist.each.with_index(1) do |artist,index|
-      puts "#{index}. #{artist.name}"
-    end
-  end
-
-  def list_genres
-    #prints all genres in the music library in a numbered list (alphabetized by genre name)
-    songs_sorted_by_genre = Genre.all.sort_by do |genre|
-      genre.name
-    end
-    songs_sorted_by_genre.each.with_index(1) do |genre,index|
-      puts "#{index}. #{genre.name}"
+   def list_genre
+    puts "Enter genre"
+    specific_genre = gets.chomp
+    if Genre.find_by_name(specific_genre) != nil
+      Genre.find_by_name(specific_genre).songs.each {|song| puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"}
+    else
+      puts "Genre does not exist"
     end
   end
-
-
-  def list_songs_by_artist
-    #prints all songs by a particular artist in a numbered list (alphabetized by song name)
-    puts "Please enter the name of an artist:"
-    input = gets.chomp
-    if artist = Artist.find_by_name(input) #find artist that matches input
-      #get the list of songs and collect a new list that is alphabetized by song name
-      songs_sorted_by_name = artist.songs.sort_by do |song|
-        song.name
-      end
-      #print the each item in the list
-      songs_sorted_by_name.each.with_index(1) do |song,index|
-        puts "#{index}. #{song.name} - #{song.genre.name}"
-      end
-    end
-  end
-
-  def list_songs_by_genre
-    #prints all songs by a particular genre in a numbered list (alphabetized by song name)
-    puts "Please enter the name of a genre:"
-    input = gets.chomp
-    if genre = Genre.find_by_name(input) #find genre that matches input
-      #get the list of songs and collect a new list that is alphabetized by song name
-      songs_sorted_by_name = genre.songs.sort_by do |song|
-        song.name
-      end
-      #print the each item in he list
-      songs_sorted_by_name.each.with_index(1) do |song,index|
-        puts "#{index}. #{song.artist.name} - #{song.name}"
-      end
-    end
-  end
-
-  def play_song
-    puts "Which song number would you like to play?"
-    list_of_songs =  Song.all.sort{ |a, b| a.name <=> b.name }
-
-    input = gets.strip.to_i
-    #if (input > 0) && (input <= list_of_songs.size)
-    #if (input >= 1) && (input <= list_of_songs.size)
-    if (1..Song.all.length).include?(input)
-      song = list_of_songs[input+2]
-      puts "Playing #{song.name} by #{song.artist.name}"
-    end
-  end
-
 end
