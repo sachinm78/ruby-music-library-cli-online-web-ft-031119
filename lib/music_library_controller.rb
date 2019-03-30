7abf58f9148eaf7f580dbb7fe8284b5f842a58a7
Sachin Mahajan <sachinm78@gmail.com>
2019-03-30 00:15:34 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -41,7 +41,7 @@ class MusicLibraryController
   end
 
   def list_songs
-    Song.all.each_with_index {|song,index|puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
+    Song.all.each_with_index {|song,index| puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
   end
 
    def list_artists
