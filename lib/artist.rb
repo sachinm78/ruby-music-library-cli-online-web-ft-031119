abcb39f0b2c267d7dfeecd37da73b9346b006ef7
Sachin Mahajan <sachinm78@gmail.com>
2019-03-30 00:00:26 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,41 +1,41 @@
 class Artist
   extend Concerns::Findable
-  attr_accessor :name, :songs, :genre
-  @@all = []
 
-  def initialize(name)
+   attr_accessor :name, :songs
+
+   @@all = []
+
+   def initialize (name)
     @name = name
     @songs = []
   end
 
-  def save
-    @@all << self
-  end
-  
-  def add_song(song)
+   def add_song (song)
+    @songs << song unless @songs.include? song
     song.artist = self unless song.artist == self
-    @songs << song unless @songs.include?(song)
-  end
-  
-  def genres
-    genres = @songs.collect do |song|
-      song.genre
-    end
-    genres.uniq
-  end
-  
-  def self.all
-    @@all
-  end
-  
-  def self.destroy_all
-    @@all.clear
   end
 
-  def self.create(name)
-    artist = self.new(name)
+   def genres
+    self.songs.map{|song| song.genre}.uniq 
+  end
+
+   def self.all
+    @@all
+  end
+
+   def self.destroy_all
+    @@all = []
+  end
+
+   def save
+    @@all << self
+  end
+
+   def self.create (name)
+    artist = Artist.new(name)
     artist.save
     artist
   end
-  
-end
+end 
+     
+9  lib/concerns/findable.rb
